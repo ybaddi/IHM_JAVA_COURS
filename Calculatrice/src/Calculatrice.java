fcc693cdabc960336916f78ad6c66b12686f6d81
youssef baddi <baddiyoussef85@gmail.com>
2025-02-17 12:03:01 +0100

    add two jpanels

--- Calculatrice/src/Calculatrice.java
+++ Calculatrice/src/Calculatrice.java
@@ -3,94 +3,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Calculatrice extends JPanel implements ActionListener {
+public class Calculatrice extends JFrame {
 
-    JTextField operationText ;
-    Double operand1, operand2;
+
+    private CalculatriceBasic calculatriceBasic;
+    private CalculatriceScientific calculatriceScientific;
+
     Calculatrice(){
-        this.setLayout(new BorderLayout());
-        operationText = new JTextField();
-        this.add(operationText, BorderLayout.NORTH);
-
-
-
-        JPanel buttonsPAnel = new JPanel();
-
-
-
-        buttonsPAnel.setLayout(new GridLayout(4,4,10,10));
-        JButton oneButton = new JButton("1");
-        oneButton.addActionListener(this);
-        buttonsPAnel.add(oneButton);
-        JButton twoButton = new JButton("2");
-        twoButton.addActionListener(this);
-        buttonsPAnel.add(twoButton);
-
-        JButton threeButton = new JButton("3");
-        threeButton.addActionListener(this);
-        buttonsPAnel.add(threeButton);
-
-        JButton divButton = new JButton("/");
-        divButton.addActionListener(this);
-        buttonsPAnel.add(divButton);
-
-        JButton fourButton = new JButton("4");
-        fourButton.addActionListener(this);
-        buttonsPAnel.add(fourButton);
-
-        JButton fiveButton = new JButton("5");
-        fiveButton.addActionListener(this);
-        buttonsPAnel.add(fiveButton);
-
-        JButton sixButton = new JButton("6");
-        sixButton.addActionListener(this);
-        buttonsPAnel.add(sixButton);
-
-        JButton prodButton = new JButton("*");
-        prodButton.addActionListener(this);
-        buttonsPAnel.add(prodButton);
-
-        JButton sevenButton = new JButton("7");
-        sevenButton.addActionListener(this);
-        buttonsPAnel.add(sevenButton);
-
-        JButton eightButton = new JButton("8");
-        eightButton.addActionListener(this);
-        buttonsPAnel.add(eightButton);
-
-        JButton nineButton = new JButton("9");
-        nineButton.addActionListener(this);
-        buttonsPAnel.add(nineButton);
-
-        JButton minisButton = new JButton("-");
-        minisButton.addActionListener(this);
-        buttonsPAnel.add(minisButton);
-
-        JButton cleanButton = new JButton("C");
-        cleanButton.addActionListener(this);
-        buttonsPAnel.add(cleanButton);
-
-        JButton zeroButton = new JButton("0");
-        zeroButton.addActionListener(this);
-        buttonsPAnel.add(zeroButton);
-
-        JButton virguleButton = new JButton(",");
-        virguleButton.addActionListener(this);
-        buttonsPAnel.add(virguleButton);
-
-        JButton plusButton = new JButton("+");
-        plusButton.addActionListener(this);
-        buttonsPAnel.add(plusButton);
-        this.add(buttonsPAnel, BorderLayout.CENTER);
-
-        JButton equalButton = new JButton("=");
-        equalButton.addActionListener(this);
-        equalButton.setBackground(Color.BLUE);
-        this.add(equalButton, BorderLayout.SOUTH);
-    }
-
-    public static void main(String[] args) {
-        JFrame calculatrice = new JFrame("Calculatrice");
+        setTitle("Calculatrice Basic");
+        setSize(300, 400);
         JMenuBar menuBar = new JMenuBar();
         JMenu menuMode = new JMenu("Mode");
         JMenuItem basicItem = new JMenuItem("Basic");
@@ -99,34 +20,35 @@ public class Calculatrice extends JPanel implements ActionListener {
         menuMode.add(basicItem);
         menuMode.add(scientificItem);
         menuBar.add(menuMode);
-        calculatrice.setJMenuBar(menuBar);
-        calculatrice.setSize(300, 400);
-        calculatrice.add(new Calculatrice());
+        setJMenuBar(menuBar);
+
+        calculatriceBasic = new CalculatriceBasic();
+        add(calculatriceBasic,BorderLayout.CENTER);
+        calculatriceScientific = new CalculatriceScientific();
+
+        basicItem.addActionListener(event -> switchMode("Basic"));
+        scientificItem.addActionListener(event -> switchMode("Scientific"));
+
+    }
+
+    private void switchMode(String mode) {
+        getContentPane().removeAll();
+        if(mode == "Basic"){
+            System.out.println("basic");
+            add(calculatriceBasic,BorderLayout.CENTER);
+        }else{
+            System.out.println("not basic");
+            add(calculatriceScientific,BorderLayout.CENTER);
+        }
+        revalidate();
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        Calculatrice calculatrice = new Calculatrice();
+
         calculatrice.setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-       String textButton =  ((JButton)e.getSource()).getText();
-        System.out.println(textButton);
-        switch (textButton){
-            case "+":
-                operand1=Double.parseDouble(operationText.getText());
-            case "-":
-                operand1=Double.parseDouble(operationText.getText());
-            case "/":
-                operand1=Double.parseDouble(operationText.getText());
-            case "*":
-                operand1=Double.parseDouble(operationText.getText());
-            case "C":
-                operationText.setText("");
-            case ",":
-                operand1=Double.parseDouble(operationText.getText());
-            case "=":
-                operand2=Double.parseDouble(operationText.getText());
-                operationText.setText(String.valueOf(operand2+operand1));
-            default:
-                operationText.setText(operationText.getText()+textButton);
-        }
-    }
+
 }
